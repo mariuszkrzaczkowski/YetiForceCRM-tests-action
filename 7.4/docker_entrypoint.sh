e0a5ae664d2393aca4cdff162b88faa1e300fa2a
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-03-26 21:24:36 +0100

    dsda

--- 7.4/docker_entrypoint.sh
+++ 7.4/docker_entrypoint.sh
@@ -29,7 +29,7 @@ php /var/www/html/tests/setup/docker_post_install.php
 service mysql start;
 service cron start
 /usr/sbin/nginx -g "daemon off;" &
-/etc/init.d/php$INPUT_PHP-fpm start
+/etc/init.d/php$PHP_VER-fpm start
 /usr/bin/mysqld_safe
 
 
